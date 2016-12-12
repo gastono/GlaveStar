1258e88cd27faf8315f799fb551446ab1fa4f9c8
Guido Gaston Ortigoza <gastonqat6@gmail.com>
2016-12-12 19:12:01 -0300

    first commit

---
+++ GlaveStar/Controllers/UserAccountController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Http;
+
+namespace GlaveStar.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/userAccount")]
+    public class UserAccountController : ApiController
+    {
+        [Route("")]
+        public IEnumerable<object> GetShipments()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+
+            return identity.Claims.Select(c => new
+            {
+                Type = c.Type,
+                Value = c.Value
+            });
+        }
+    }
+}
